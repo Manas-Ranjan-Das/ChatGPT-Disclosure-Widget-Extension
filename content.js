b3e7d742c94fa885d4f7826f3763b88217df1dd8
Manas Ranjan Das <manas.das@smartrecontech.com>
2025-10-06 11:32:11 +0530

    Bug Fix : Fixes bug where multiple details elements appear nested at the end of response loading

--- content.js
+++ content.js
@@ -1,14 +1,19 @@
 console.log("Content script loaded.");
 
+var extLogs = [];
+
 setInterval(
     function timeoutHandler(params) {
-        document.querySelectorAll(".markdown:not(.processed)").forEach(makeDisclosable);
+        const elements = document.querySelectorAll('.markdown:not(.processed):not(details .markdown)').forEach(makeDisclosable);
+        
     },
     5000
 )
 
 
 function makeDisclosable(element) {
+  extLogs.push(element);
+  
   if (!element) return;
 
   const summaryText = ( element.innerText.trim().slice(0, 49) || "[empty]" ) + ' . . .';
